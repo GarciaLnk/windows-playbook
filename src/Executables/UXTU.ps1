f71ad63a28f9a90a78e9124b3de7d1cd3e4e5bf1
Alberto García <4411661+GarciaLnk@users.noreply.github.com>
2023-12-31 03:27:54 +0100

    fix: ps1 scripts with runas options

--- src/Executables/UXTU.ps1
+++ src/Executables/UXTU.ps1
@@ -2,6 +2,7 @@
 $url = "https://github.com/JamesCJ60/Universal-x86-Tuning-Utility/releases/latest/download/Universal.x86.Tuning.Utility.V2.msi"
 $tempPath = Join-Path -Path $env:TEMP -ChildPath "Universal.x86.Tuning.Utility.V2.msi"
 
+$ProgressPreference = 'SilentlyContinue'
 Invoke-WebRequest -Uri $url -OutFile $tempPath
 Start-Process -FilePath "msiexec.exe" -ArgumentList "/i `"$tempPath`" /passive /norestart" -Wait -NoNewWindow
 Remove-Item -Path $tempPath -Force
--- src/Executables/WSA.ps1
+++ src/Executables/WSA.ps1
@@ -1,24 +1,20 @@
 $url = "https://github.com/MustardChef/WSABuilds/releases/download/Windows_11_2310.40000.2.0/WSA_2310.40000.2.0_x64_Release-Nightly-with-Magisk-26.4-stable-MindTheGapps-13.0.7z"
 $tempPath = "$env:TEMP\WSA_2310.40000.2.0.7z"
 $extractPath = "$env:TEMP\WSA_2310.40000.2.0_x64"
+$destinationPath = [System.IO.Path]::Combine([System.Environment]::GetFolderPath('MyDocuments'), "WSA")
 
-if (-not (Get-Command "7z.exe" -ErrorAction SilentlyContinue)) {
-    Write-Error "7-Zip is not installed or not in the system's PATH. Please install it and try again."
+$7zipPath = "C:\Program Files\7-Zip\7z.exe"
+if (-not (Test-Path $7zipPath)) {
+    Write-Error "7-Zip is not installed or not in the expected location. Please install it and try again."
     exit
 }
 
+$ProgressPreference = 'SilentlyContinue'
 Invoke-WebRequest -Uri $url -OutFile $tempPath
-
-# Extract the .7z archive
-7z x "$tempPath" -o"$env:TEMP"
+& $7zipPath x "$tempPath" -o"$env:TEMP" -y
 Remove-Item -Path $tempPath
 
-# Move the newly extracted folder to the Documents directory
-$destinationPath = [System.IO.Path]::Combine([System.Environment]::GetFolderPath('MyDocuments'), "WSA")
+if (Test-Path $destinationPath) {
+    Remove-Item -Path $destinationPath -Recurse -Force
+}
 Move-Item -Path $extractPath -Destination $destinationPath
-
-Push-Location -Path $destinationPath
-Start-Process "Run.bat" -Wait
-
-Pop-Location
-exit
